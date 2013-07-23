3a4cf223316ba8ace4a054f7aeddde286584435d
jasonrwagner <wagner.jason.r@gmail.com>
2013-07-23 18:45:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubble::Application.config.secret_token = '54417d1200079830009d058f93898df7dd51a1971aab82770026e97feac5ee92398e6071f766685a423111ba69b319c7657bd1c3f459592dcc8b50b00de4e09c'
