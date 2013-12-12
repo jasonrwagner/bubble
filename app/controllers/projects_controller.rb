cd142b79adaaf9a15f8beae275fa7db16e5558d8
Jason Wagner <wagner.jason.r@gmail.com>
2013-12-12 15:23:09 -0500

    added update action to project edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,7 +30,7 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
-    if  @project.update_attributes(params[:name])
+    if  @project.update_attributes(project_params)
       redirect_to @project
     else
       render 'edit'
