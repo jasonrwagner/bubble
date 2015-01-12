d057c016e0f05f7e99b7badba4e1e76397684134
Devin Brown <devinbrown7@gmail.com>
2015-01-12 18:31:25 -0500

    Fixed edit pages for Project and Profile
    Also,
    - Added projects to calendar
    - Styled the edit pages
    - Added more links to content throughout the pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,29 +2,37 @@ class ProfilesController < ApplicationController
 
   def index
     @profile = Profile.all
-  	#@users = User.all
+    #@users = User.all
   end
 
   def show
-  	@profile = Profile.find(params[:id])
+    @profile = Profile.find(params[:id])
   end
 
   def edit
-  	@profile = Profile.find(params[:id])
+    @profile = Profile.find(params[:id])
   end
 
   def new
-  	@profile = Profile.new
+    @profile = Profile.new
   end
 
   def create
     @profile = Profile.new(profile_params)
-      if @profile.save
-        flash[:success] = "New Profile"
-        redirect_to pages_path
-      else
-        render :new
-      end
+    if @profile.save
+      flash[:success] = "New Profile"
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update_attributes(profile_params)
+      redirect_to pages_path
+    else
+      render 'edit'
     end
   end
 
@@ -34,3 +42,4 @@ class ProfilesController < ApplicationController
     params.require(:profile).permit(:first_name, :last_name, :likes, :about_me)
   end
 end
+
